abc7921fd6c156aceaac136b59f4b47e684b655b
Jackson <armnellgomez@gmail.com>
2023-12-20 00:01:24 -0500

    Agregue el texto en ¿Quien soy? y corregi estilos.

--- src/componentes/QuienSoy.js
+++ src/componentes/QuienSoy.js
@@ -8,7 +8,15 @@ function QuienSoy() {
 				<div className='quien-soy'>
 					<div className='quien-soy-container'>
 						<p className='quien-soy-text'>
-							EN CONSTRUCCION...
+							Comencé mi camino en el desarrollo web en el año en el 2021, al comienzo fue muy lento ya que mi trabajo me dejaba muy poco tiempo para poder estudiar y practicar.
+							<br /><br />
+							Aunque soy relativamente nuevo en todo esto, ya he adquirido conocimientos en tecnologías como React.js, Bootstrap, Git y GitHub, AWS, y he explorado el diseño con Figma. Sin olvidar HTML y CSS que es por donde muchos decidimos comenzar.
+							<br /><br />
+							Mi enfoque siempre ha sido aprender y crecer constantemente, cada desafío que tomo lo llevo con constancia y paso a paso, como ya dije, seguiré desarrollando mis habilidades estudiando y practicando con la construcción de proyectos web propios y de terceros.
+							<br /><br />
+							Como dijo Muhammad Ali…
+							<br />
+							“No cuentes los días, haz que los días cuenten.”
 						</p>
 					</div>
 				</div>
