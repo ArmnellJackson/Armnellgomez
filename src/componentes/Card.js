2cba4cad0555383358174250da107bd648291314
Jackson <armnellgomez@gmail.com>
2023-12-24 20:01:12 -0500

    Modifique estilos en las card y en el formulario de registro.

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -4,14 +4,14 @@ import '../style/Card.css';
 function Card({ onClick, href, img, alt, text }) {
     return (
         <>
-            <div className="style-card" onClick={onClick}>
+            <article className="style-card" onClick={onClick}>
                 <a href={href} target="_blank" rel="noopener noreferrer">
                     <img src={img} className="card-img-top" alt={alt} />
                 </a>
                 <div className="card-body">
                     <p className="card-text">{text}</p>
                 </div>
-            </div>
+            </article>
         </>
     );
 }
