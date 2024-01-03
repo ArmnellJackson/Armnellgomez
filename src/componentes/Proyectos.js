d4fe92f1bad1e07908665df5c9d03807ee448681
Jackson <armnellgomez@gmail.com>
2024-01-03 07:20:44 -0500

    Cree la card del proyecto de UnaAmigaManicurista.

--- src/componentes/Proyectos.js
+++ src/componentes/Proyectos.js
@@ -4,6 +4,7 @@ import Card from './Card';
 import ImgFreeCodeCamp from '../img/freeCodeCamp-logo.jpeg';
 import ImgContadorClicks from '../img/Contador-de-Clicks.png';
 import Calculadora from '../img/calculadora-react.png';
+import UnaAmigaImaginaria from '../img/unaAmigaManicurista.png';
 
 function Proyectos() {
 
@@ -12,26 +13,33 @@ function Proyectos() {
 			<main>
 				<section className="proyectos">
 
-				<Card
-				href='https://armnellgomez-clone-testimonios-freecodecamp-react.s3.amazonaws.com/index.html'
-				img={ImgFreeCodeCamp}
-				alt='logo freecodecamp'
-				text='Pagina de testimonios de FreeCodeCamp. En este proyecto practique el uso de las props.'
-			/>
+					<Card
+						href='https://armnellgomez-clone-testimonios-freecodecamp-react.s3.amazonaws.com/index.html'
+						img={ImgFreeCodeCamp}
+						alt='logo freecodecamp'
+						text='Pagina de testimonios de FreeCodeCamp. En este proyecto practique el uso de las props.'
+					/>
 
-			<Card
-				href='https://armnellgomez-contador-de-clics-react.s3.amazonaws.com/index.html'
-				img={ImgContadorClicks}
-				alt='contador de clicks'
-				text='Esto es un contador de clicks donde practique el uso de useState.'
-			/>
+					<Card
+						href='https://armnellgomez-contador-de-clics-react.s3.amazonaws.com/index.html'
+						img={ImgContadorClicks}
+						alt='contador de clicks'
+						text='Esto es un contador de clicks donde practique el uso de useState.'
+					/>
 
-			<Card
-				href='https://armnellgomez-calculadora-react.s3.amazonaws.com/index.html'
-				img={Calculadora}
-				alt='calculadora'
-				text='Calculadora.'
-			/>
+					<Card
+						href='https://armnellgomez-calculadora-react.s3.amazonaws.com/index.html'
+						img={Calculadora}
+						alt='calculadora'
+						text='Calculadora.'
+					/>
+
+					<Card
+						href='https://unaamigamanicurista.github.io/UnaAmigaManicurista/'
+						img={UnaAmigaImaginaria}
+						alt='blog de uñas'
+						text='Blog Manicurista. (En construccion)'
+					/>
 				</section>
 			</main>
 		</>
